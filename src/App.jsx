f21eac03405dd12cbb2accf9296fe06b141c8d05
Crass_Crest_Chris_Cross_Crust <serenochrisclyde@gmail.com>
2025-11-20 10:53:51 +0800

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState, useEffect } from "react";
 import TimeInput from "./Input.jsx";
 import TimerDisplay from "./Display.jsx";
 import StartButton from "./Start.jsx";
+import alarm from "./assets/alarm.mp3"
 
 function App() {
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
+  const audio = new Audio(alarm);
 
   const [totalTime, setTotalTime] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
@@ -20,6 +22,10 @@ function App() {
       timer = setInterval(() => {
         setTotalTime((prev) => prev - 1);
       }, 1000);
+    }else if(isRunning && totalTime==0){
+      audio.play();
+      audio.loop = true;
+
     }
 
     return () => clearInterval(timer);
@@ -40,10 +46,14 @@ function App() {
   };
 
   const handleResume = () => {
+    
     setIsPaused(false);
   };
 
   const handleReset = () => {
+    audio.pause();
+    audio.loop = false;
+    audio.currentTime = 0;
     setIsRunning(false);
     setIsPaused(false);
     setTotalTime(0);
@@ -60,7 +70,7 @@ function App() {
 
   return (
     <div className="container">
-      <h2>⏳ React Countdown Timer</h2>
+      <h2>⏳ React Countdown Timer ⏳</h2>
 
       <div className="inputs">
         <TimeInput label="Hours" value={hours} onChange={setHours} />
@@ -72,19 +82,27 @@ function App() {
         <button onClick={handleStart}>Start Timer</button>
       )}
 
-      {isRunning && !isPaused && (
+      {isRunning && !isPaused && totalTime!= 0 &&(
         <button onClick={handlePause}>Pause</button>
       )}
 
-      {isRunning && isPaused && (
+      {isRunning && isPaused &&(
         <button onClick={handleResume}>Resume</button>
       )}
 
-      {isRunning && (
+      {isRunning && totalTime!= 0 &&(
         <button onClick={handleReset}>Reset</button>
       )}
 
+      {isRunning && totalTime == 0 && !isPaused && (
+        <button onClick={handleReset}>Stop Alarm</button>
+        )
+      }
+
       <TimerDisplay time={timeObj} />
+      {isRunning && totalTime == 0 && !isPaused && (
+        <h3 className="alarm-text">⏰ Time's up! ⏰</h3>
+      )}
     </div>
   );
 }
